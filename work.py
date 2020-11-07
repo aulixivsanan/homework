415b347cc018eb79c0274075200f7c964fbffa64
王志强 <kuxiemotion@163.com>
2020-11-08 03:35:18 +0800

    笔记本删除test01提交

--- work.py
+++ work.py
@@ -8,5 +8,11 @@ class Add(unittest.TestCase):
     def test06(self):
         print('我爱你')
 
+    def test05(self):
+        print('我爱你')
+
+    def test06(self):
+        print('我爱你')
+
 if __name__ == '__main__':
     unittest.main()
