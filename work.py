da5ec652203d660116348621153058dc26e12660
Administrator <18611435237@163.com>
2020-11-08 03:15:34 +0800

    不知test01已删除，新增test04提交

--- work.py
+++ work.py
@@ -2,13 +2,13 @@ import unittest
 
 class Add(unittest.TestCase):
 
+    def test01(self):
+        print('我爱你')
+
     def test02(self):
         print('我爱你')
 
     def test03(self):
-        print('我爱你')
-
-    def test04(self):
         print('我爱你')
 
 if __name__ == '__main__':
